5f4e65c1783742851044376cc1b57b3950380d48
LYingYuan <luoyyuan69@gmail.com>
2023-12-13 14:02:27 +0800

    feat: remove all code

--- src/utils/validation.ts
+++
@@ -1,5 +0,0 @@
-import { isUri } from "valid-url";
-
-export function isValidUrlArray(urls: any): urls is string[] {
-  return Array.isArray(urls) && urls.every(url => typeof url === "string" && isUri(url));
-}
